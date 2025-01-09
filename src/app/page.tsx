d6f863b24c12aa42b4ee85d97cf51ffb9cd6b3dd
Timur Gorodetsky <gorodetsky1392@gmail.com>
2025-01-09 12:00:38 +0500

    add:title and categories comp and fix exports in shared

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,7 +1,12 @@
-import { Button } from '@/components/ui/button';
+import { Categories, Container, Title } from '@/components/shared';
 
 export default function Home() {
-  return <>
-  <h1>Home Page</h1>
-  </>;
+  return (
+    <>
+      <Container className='mt-6'>
+        <Title text='Все пиццы' size='lg' className='font-extrabold'/>
+        <Categories/>
+      </Container>
+    </>
+  );
 }
